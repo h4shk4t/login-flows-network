0ab786e0000188740f132d305d2c3b07f651a3ad
Ashutosh <cyberboy9971@gmail.com>
2023-07-31 14:41:51 +0530

    Add functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,27 @@ use lamba_flows::{request_received, send_response};
 use flowsnet_platform_sdk::logger;
 use std::collections::HashMap;
 use serde_json::Value;
+
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+pub async fn run() -> anyhow::Result<()> {
+    request_received(|headers, qry, body| {
+        handler(headers, qry, body)
+    }).await;
+    Ok(())
+}
+
+async fn handler(headers: Vec<(String, String)>, qry: HashMap<String, Value>, _body: Vec<u8>) {
+    logger::init();
+    log::info!("Headers -- {:?}", headers);
+
+    let msg = qry.get("msg").unwrap();
+    // let msg = String::from_utf8(body).unwrap_or("".to_string());
+    let resp = format!("Welcome to flows.network.\nYou just said: '{}'.\nLearn more at: https://github.com/flows-network/hello-world\n", msg);
+
+    send_response(
+        200,
+        vec![(String::from("content-type"), String::from("text/html"))],
+        resp.as_bytes().to_vec(),
+    );
+}
